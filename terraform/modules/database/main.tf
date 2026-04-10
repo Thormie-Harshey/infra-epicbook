c63b631d205c82926e89d91d35fafe7242201a81
Ashaye Adetomiwa <ashayeadetomiwa77@gmail.com>
2026-04-10 13:40:58 +0100

    changed database to free tier

--- terraform/modules/database/main.tf
+++ terraform/modules/database/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_mysql_flexible_server" "db" {
   location               = var.location
   administrator_login    = "adminuser"
   administrator_password = var.db_password
-  sku_name               = "B_Gen5_1"
+  sku_name               = "B_Standard_B1ms"
   version                = "8.0.21"
 
   delegated_subnet_id = var.subnet_id
